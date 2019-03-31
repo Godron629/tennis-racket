55a05397a8f182401d56ceec8cb63a9012d131a1
Gideon <gideonrich@hotmail.ca>
2019-03-30 21:23:10 -0600

    Finished docstrings

--- startEval.rkt
+++ startEval.rkt
@@ -7,16 +7,12 @@
  dict-get
  dict-update
  dict-update-rec
- apply-binary-op
- apply-unary-op
+ binary-operator
+ unary-operator
  evaluate-expression-rec
- apply-value-op
+ operator
  handle-if
- formals
- body
- funpart
- envpart
- apply-closure
+ closure
  get-first-of-all
  get-second-of-all
  convert-let
@@ -47,7 +43,7 @@
 ;; return list
 (define (mk-list-of-three item1 item2 item3)
   (cons item1 (cons item2 (cons item3 '()))))
-
+ 
 ;; Modify racket's `assoc` to work like Python's dict.get()
 ;;   by returning only the value of a given key instead of the
 ;;   key value pair. If the key does not exist, return False.
@@ -100,14 +96,14 @@
        (cdr values)
        (dict-update (car keys) (car values) dict))))
 
-;; Apply function with name f to parameters x and y
+;; Apply binary function with name f to parameters x and y
 ;;
 ;; param symbol f
 ;; param item x
 ;; param item y
 ;;
 ;; return any
-(define (apply-binary-op f x y)
+(define (binary-operator f x y)
   (cond
     ((equal? f 'cons) (cons x y))
     ((equal? f '+) (+ x y))
@@ -122,13 +118,13 @@
     ((equal? f 'equal?) (equal? x y))
     (else (error "apply-binary: operator not supported" f))))
 
-;; Apply function with name f to parameter x
+;; Apply unary function with name f to parameter x
 ;;
 ;; param symbol f
 ;; param item x
 ;;
 ;; return any
-(define (apply-unary-op f x)
+(define (unary-operator f x)
   (cond
     ((equal? f 'car) (car x))
     ((equal? f 'cdr) (cdr x))
@@ -150,13 +146,25 @@
        (*startEval (car exp) env)
        (evaluate-expression-rec (cdr exp) env))))
 
-;; Apply primop to args
-(define (apply-value-op primop args)
-  (if (equal? (length args) 1)
-      (apply-unary-op (cadr primop) (car args))
-      (apply-binary-op (cadr primop) (car args) (cadr args))))
+;; Apply operator to args depending on number of parameters
+;;
+;; param symbol op
+;; param list args
+;;
+;; return any
+(define (operator op args)
+  (if (> (length args) 1)
+      ;; Then we have more than one argument
+      (binary-operator (cadr op) (car args) (cadr args))
+      ;; Else it's a single argument
+      (unary-operator (cadr op) (car args))))
 
-;; Check predicate and return consequent or alternative
+;; Check predicate and return result of consequent or alternative
+;;
+;; param list exp
+;; param list env
+;;
+;; return any
 (define (handle-if exp env)
   (let ((result (*startEval (cadr exp) env)))
     (if (or (null? result) (not result))
@@ -165,75 +173,141 @@
       ;; Predicate is True, execute the consequent
       (*startEval (caddr exp) env))))
 
-;; Get the formal parameter names from a lambda
-(define (formals exp) (cadr exp))
-;; Get the body of a lambda
-(define (body exp) (caddr exp))
-;; Get the arguments from a closure
-(define (funpart clo) (cadr clo))
-;; Get the environment from a closure
-(define (envpart clo) (caddr clo))
+;; Make a closure, all we need to do is make
+;; it identifiable as a closure and wrap the
+;; lambda and the environment up with it.
+;;
+;; param list exp
+;; param list env
+;;
+;; return list
+(define (mk-closure exp env)
+  (mk-list-of-three 'closure exp env))
+
+;; Make a list of identifiable basic racket operators
+;; (to tell apart from closures).
+;;
+;; param list ops
+;;
+;; return list
+(define (mk-default-operators ops)
+  (cond
+    ((null? ops) '())
+    (else (cons
+           (mk-list-of-two
+            (car ops)
+            (mk-list-of-two 'operator (car ops)))
+           (mk-default-operators (cdr ops))))))
 
 ;; Apply a closure to arguments
-(define (apply-closure clo args)
-  (*startEval (body (funpart clo))
-             (dict-update-rec (formals (funpart clo)) args (envpart clo))))
+;;
+;; param list clo
+;; param list args
+;;
+;; return any
+(define (closure clo args)
+  
+  (let* 
+    ((closure-environment (caddr clo))
+     (closure-function (cadr clo))
+     (function-body (caddr closure-function))
+     (formal-arguments (cadr closure-function)))
+  
+    (*startEval function-body  
+     ;; 'Execute' the function within the saved environment
+     (dict-update-rec
+      ;; Get a list of keys
+      formal-arguments
+      ;; Get a list values
+      args
+      ;; Combine those key values with the current environment
+      closure-environment))))
 
-;; Convert let into a lambda
-(define (convert-let lamexp)
-  (append (mk-list-of-one (mk-list-of-three 'lambda
-                (get-first-of-all (cadr lamexp))
-                (caddr lamexp)))
-         (get-second-of-all (cadr lamexp))))
+;; Instead of implementing a `let`, just convert the let
+;; into a lambda and start this evaluation over. 
+;;
+;; i.e. (let ((x 5)) x) => ((lambda (x) x) 5)
+;; 
+;; param list exp
+;;
+;; return list lambda exp
+(define (convert-let exp)
+  ;; Append the (lambda ... ) part to all the arguments
+  (append
+   (mk-list-of-one
+    (mk-list-of-three
+     'lambda
+     (get-first-of-all (cadr exp))
+     (caddr exp)))
+   (get-second-of-all (cadr exp))))
 
 ;; Get the first element of every sublist
+;;
+;; param list ls
+;;
+;; return list
 (define (get-first-of-all ls)
   (cond
     ((null? ls) '())
     ((cons (caar ls) (get-first-of-all (cdr ls))))))
 
 ;; Get the second element of every sublist
+;;
+;; param ls list
+;;
+;; return list
 (define (get-second-of-all ls)
   (cond
     ((null? ls) '())
     ((cons (cadar ls) (get-second-of-all (cdr ls))))))
 
+;; Helper function to avaluate subset of racket
+;;
+;; param list exp
+;; param list(list) env
+;;
+;; return any
 (define (*startEval exp env)
   (cond
-    ((number? exp) exp)
+    ((number? exp)
+     ;; return the number
+     exp)
     ((symbol? exp)
+     ;; if we know about this symbol,
+     ;; return it - otherwise, raise.
      (if (dict-get exp env)
          (dict-get exp env)
          (error "Symbol not defined: " exp)))
-    ((equal? (car exp) 'quote) (cadr exp))
-    ((equal? (car exp) 'let) (*startEval (convert-let exp) env))
-    ((equal? (car exp) 'lambda) (mk-list-of-three 'closure exp env))
+    ((equal? (car exp) 'quote)
+     ;; return what is inside the quote
+     (cadr exp))
+    ((equal? (car exp) 'let)
+     ;; convert the let to a lambda and reevaluate
+     (*startEval (convert-let exp) env))
+    ((equal? (car exp) 'lambda)
+     ;; make a closure and evaluate it later
+     (mk-closure exp env))
     ((equal? (car exp) 'if)
+     ;; evaluate if statement
      (handle-if (exp env)))
     (else
+     ;; If nothing else, it must be a function.
+     ;; This can be a racket function or a lambda.
      (let ((result (evaluate-expression-rec exp env)))
        (if (equal? (caar result) 'closure)
-           (apply-closure (car result) (cdr result))
-           (apply-value-op (car result) (cdr result)))))))
+           ;; evaluate closure we built earlier
+           (closure (car result) (cdr result))
+           ;; it's a racket operator!
+           (operator (car result) (cdr result)))))))
 
-
-(define operators
-   '((+ (primop +))
-     (- (primop -))
-     (* (primop *))
-     (/ (primop /))
-     (= (primop =))
-     (equal? (primop equal?))
-     (< (primop <))
-     (> (primop >))
-     (<= (primop <=))
-     (>= (primop >=))
-     (car (primop car))
-     (cdr (primop cdr))
-     (cons (primop cons))))
-
+;; Evaluate a subset of racket functionality
+;; akin to `eval` in racket.
+;;
+;; param list exp (i.e. '(+ 5 5))
+;;
+;; return any 
 (define (startEval exp)
-  (*startEval exp operators))
-
-     
-              
+  (*startEval
+   exp
+   (mk-default-operators
+    '(+ - * / = equal? < > <= >= car cdr cons))))
--- test_startEval.rkt
+++ test_startEval.rkt
@@ -48,83 +48,73 @@
   (test-case "3" (check-equal? (dict-update-rec '(1 2) '(4 5) '((1 10))) '((1 4) (2 5))))
   )
 
-(define-test-suite apply-binary-suite
-  "apply-binary-suite"
-  ;; apply-binary-op: apply f to x and y
-  (test-case "+" (check-equal? (apply-binary-op '+ 10 5) 15))
-  (test-case "-" (check-equal? (apply-binary-op '- 10 5) 5))
-  (test-case "*" (check-equal? (apply-binary-op '* 10 5) 50))
-  (test-case "/" (check-equal? (apply-binary-op '/ 10 5) 2))
-  (test-case "<" (check-equal? (apply-binary-op '< 10 5) #f))
-  (test-case "<" (check-equal? (apply-binary-op '< 5 10) #t))
-  (test-case "<" (check-equal? (apply-binary-op '< 10 10) #f))
-  (test-case ">" (check-equal? (apply-binary-op '> 10 5) #t))
-  (test-case ">" (check-equal? (apply-binary-op '> 5 10) #f))
-  (test-case ">" (check-equal? (apply-binary-op '> 10 10) #f))
-  (test-case ">=" (check-equal? (apply-binary-op '>= 10 5) #t))
-  (test-case ">=" (check-equal? (apply-binary-op '>= 5 10) #f))
-  (test-case ">=" (check-equal? (apply-binary-op '>= 10 10) #t))
-  (test-case "<=" (check-equal? (apply-binary-op '<= 10 5) #f))
-  (test-case "<=" (check-equal? (apply-binary-op '<= 5 10) #t))
-  (test-case "<=" (check-equal? (apply-binary-op '<= 10 10) #t))
-  (test-case "=" (check-equal? (apply-binary-op '= 10 10) #t))
-  (test-case "equal?" (check-equal? (apply-binary-op 'equal? "hi" "hi") #t))
-  (test-case "equal?" (check-equal? (apply-binary-op 'equal? "hi" "bye") #f))
+(define-test-suite binary-operator-suite
+  "binary-operator-suite"
+  ;; binary-operator: apply f to x and y
+  (test-case "+" (check-equal? (binary-operator '+ 10 5) 15))
+  (test-case "-" (check-equal? (binary-operator '- 10 5) 5))
+  (test-case "*" (check-equal? (binary-operator '* 10 5) 50))
+  (test-case "/" (check-equal? (binary-operator '/ 10 5) 2))
+  (test-case "<" (check-equal? (binary-operator '< 10 5) #f))
+  (test-case "<" (check-equal? (binary-operator '< 5 10) #t))
+  (test-case "<" (check-equal? (binary-operator '< 10 10) #f))
+  (test-case ">" (check-equal? (binary-operator '> 10 5) #t))
+  (test-case ">" (check-equal? (binary-operator '> 5 10) #f))
+  (test-case ">" (check-equal? (binary-operator '> 10 10) #f))
+  (test-case ">=" (check-equal? (binary-operator '>= 10 5) #t))
+  (test-case ">=" (check-equal? (binary-operator '>= 5 10) #f))
+  (test-case ">=" (check-equal? (binary-operator '>= 10 10) #t))
+  (test-case "<=" (check-equal? (binary-operator '<= 10 5) #f))
+  (test-case "<=" (check-equal? (binary-operator '<= 5 10) #t))
+  (test-case "<=" (check-equal? (binary-operator '<= 10 10) #t))
+  (test-case "=" (check-equal? (binary-operator '= 10 10) #t))
+  (test-case "equal?" (check-equal? (binary-operator 'equal? "hi" "hi") #t))
+  (test-case "equal?" (check-equal? (binary-operator 'equal? "hi" "bye") #f))
   )
 
-(define-test-suite apply-unary-suite
-  "apply-unary test suite"
-  ;; apply-unary-op: apply f to x
-  (test-case "car" (check-equal? (apply-unary-op 'car '(1 2 3)) 1))
-  (test-case "cdr" (check-equal? (apply-unary-op 'cdr '(1 2 3)) '(2 3)))
-  (test-case "cdr" (check-equal? (apply-unary-op 'cdr '(1)) '()))
-  (test-case "pair?" (check-equal? (apply-unary-op 'pair? '(1 2)) #t))
-  (test-case "pair?" (check-equal? (apply-unary-op 'pair? '()) #f))
-  (test-case "pair?" (check-equal? (apply-unary-op 'pair? '1) #f))
+(define-test-suite unary-operator-suite
+  "unary-operator test suite"
+  ;; unary-operator: apply f to x
+  (test-case "car" (check-equal? (unary-operator 'car '(1 2 3)) 1))
+  (test-case "cdr" (check-equal? (unary-operator 'cdr '(1 2 3)) '(2 3)))
+  (test-case "cdr" (check-equal? (unary-operator 'cdr '(1)) '()))
+  (test-case "pair?" (check-equal? (unary-operator 'pair? '(1 2)) #t))
+  (test-case "pair?" (check-equal? (unary-operator 'pair? '()) #f))
+  (test-case "pair?" (check-equal? (unary-operator 'pair? '1) #f))
   )
 
-(define-test-suite apply-value-op-suite
-  "apply-value-op test suite"
-  ;; apply-value-op: apply operator to arguments
-  (test-case "+" (check-equal? (apply-value-op '(primop +) '(1 2)) 3))
-  (test-case "-" (check-equal? (apply-value-op '(primop -) '(1 2)) -1))
-  (test-case "equal?" (check-equal? (apply-value-op '(primop equal?) '(1 2)) #f))
-  (test-case "<" (check-equal? (apply-value-op '(primop <) '(1 2)) #t))
+(define-test-suite operator-suite
+  "operator test suite"
+  ;; operator: apply operator to arguments
+  (test-case "+" (check-equal? (operator '(operator +) '(1 2)) 3))
+  (test-case "-" (check-equal? (operator '(operator -) '(1 2)) -1))
+  (test-case "equal?" (check-equal? (operator '(operator equal?) '(1 2)) #f))
+  (test-case "<" (check-equal? (operator '(operator <) '(1 2)) #t))
   )
 
 (define-test-suite evaluate-expression-rec-suite
   "evaluate-expression-rec test suite"
   ;; evaluate-expression-rec: recursively evaluate expressions
-  (test-case "1" (check-equal? (evaluate-expression-rec '(+ 1 2) '((+ (primop +)))) '((primop +) 1 2)))
-  (test-case "2" (check-equal? (evaluate-expression-rec '(+ 1 x) '((+ (primop +)) (x 5))) '((primop +) 1 5)))
-  (test-case "3" (check-equal? (evaluate-expression-rec '(+ 1 (+ 2 3)) '((+ (primop +)))) '((primop +) 1 5)))
+  (test-case "1" (check-equal? (evaluate-expression-rec '(+ 1 2) '((+ (operator +)))) '((operator +) 1 2)))
+  (test-case "2" (check-equal? (evaluate-expression-rec '(+ 1 x) '((+ (operator +)) (x 5))) '((operator +) 1 5)))
+  (test-case "3" (check-equal? (evaluate-expression-rec '(+ 1 (+ 2 3)) '((+ (operator +)))) '((operator +) 1 5)))
   )
 
 (define-test-suite handle-if-suite
   "handle-if test suite"
   ;; handle-if: check result of predicate and return appropriate result
-  (test-case "true" (check-equal? (handle-if '(if (equal? 1 1) 55 99) '((equal? (primop equal?)))) 55))
-  ;(test-case "false" (check-equal? (handle-if '(if (equal? 1 2) 55 99) '((equal? (primop equal?)))) 99))
+  (test-case "true" (check-equal? (handle-if '(if (equal? 1 1) 55 99) '((equal? (operator equal?)))) 55))
+  (test-case "false" (check-equal? (handle-if '(if (equal? 1 2) 55 99) '((equal? (operator equal?)))) 99))
   )
 
-(define-test-suite lambda-parts-suite
-  "lambda parts test suite"
-  ;; funpart: return the lambda of a closure
-  (test-case "funpart" (check-equal? (funpart '(closure (lambda (x) (+ x 1)))) '(lambda (x) (+ x 1))))
-  (test-case "formals" (check-equal? (formals '(closure (lambda (x y z) (+ x y (- z 1))))) '(lambda (x y z) (+ x y (- z 1)))))
-  (test-case "formals1" (check-equal? (formals '(lambda (x) (+ x 1))) '(x)))
-  (test-case "formals2" (check-equal? (formals '(lambda (x y z) (+ x y (- z 1)))) '(x y z)))
-  (test-case "envpart" (check-equal? (envpart '(closure (lambda (x) (+ x 1)) ((+ +)))) '((+ +))))
-  )
-
-(define-test-suite apply-closure-suite
-  "apply-closure test suite"
-  ;; apply-closure: apply closure to arguments
-  (test-case "1" (check-equal? (apply-closure '(closure (lambda (x) x) ()) '(5)) 5))
-  (test-case "2" (check-equal? (apply-closure '(closure (lambda (x) (+ 1 x)) ((+ (primop +)))) '(5)) 6))
-  (test-case "3" (check-equal? (apply-closure '(closure (lambda (x) (+ x y)) ((+ (primop +)) (y 10))) '(5)) 15))
-  (test-case "4" (check-equal? (apply-closure '(closure (lambda (x y) (+ x y)) ((+ (primop +)))) '(99 99)) 198))
-  (test-case "5" (check-equal? (apply-closure '(closure (lambda (x y) (+ q p)) ((+ (primop +)) (q 10) (p 15))) '(99 99)) 25))
+(define-test-suite closure-suite
+  "closure test suite"
+  ;; closure: apply closure to arguments
+  (test-case "1" (check-equal? (closure '(closure (lambda (x) x) ()) '(5)) 5))
+  (test-case "2" (check-equal? (closure '(closure (lambda (x) (+ 1 x)) ((+ (operator +)))) '(5)) 6))
+  (test-case "3" (check-equal? (closure '(closure (lambda (x) (+ x y)) ((+ (operator +)) (y 10))) '(5)) 15))
+  (test-case "4" (check-equal? (closure '(closure (lambda (x y) (+ x y)) ((+ (operator +)))) '(99 99)) 198))
+  (test-case "5" (check-equal? (closure '(closure (lambda (x y) (+ q p)) ((+ (operator +)) (q 10) (p 15))) '(99 99)) 25))
   )
 
 (define-test-suite get-first-of-all-suite
@@ -160,8 +150,8 @@
   (test-case "5" (check-equal? (startEval '(quote (1 2 3))) '(1 2 3)))
   (test-case "6" (check-equal? (startEval '(quote (quote (quote 5)))) '''5))
   (test-case "7" (check-equal? (startEval '((lambda (x) x) 5)) 5))
-  (test-case "8" (check-equal? (*startEval '((lambda (x y) (+ x y)) 5 6) '((+ (primop +)))) 11))
-  (test-case "9" (check-equal? (*startEval '((lambda (x y) (+ p q)) 1 1) '((+ (primop +)) (p 5) (q 10))) 15))
+  (test-case "8" (check-equal? (*startEval '((lambda (x y) (+ x y)) 5 6) '((+ (operator +)))) 11))
+  (test-case "9" (check-equal? (*startEval '((lambda (x y) (+ p q)) 1 1) '((+ (operator +)) (p 5) (q 10))) 15))
   (test-case "10" (check-equal? (startEval '(let ((x 1)) x)) 1))
   (test-case "11" (check-equal? (startEval '(let ((+ (lambda (x) (cdr x))) (- '(1 2 3 4 5))) (+ -))) '(2 3 4 5)))
   (test-case "12" (check-equal? (startEval '(let ((inc (lambda (x) (+ x (quote 1))))) (inc (quote 5)))) '6))
@@ -173,13 +163,12 @@
 (run-tests dict-get-suite)
 (run-tests dict-update-suite)
 (run-tests dict-update-rec-suite)
-(run-tests apply-binary-suite)
-(run-tests apply-unary-suite)
-(run-tests apply-value-op-suite)
+(run-tests binary-operator-suite)
+(run-tests unary-operator-suite)
+(run-tests operator-suite)
 (run-tests evaluate-expression-rec-suite)
 (run-tests handle-if-suite)
-(run-tests lambda-parts-suite)
-(run-tests apply-closure-suite)
+(run-tests closure-suite)
 (run-tests get-first-of-all-suite)
 (run-tests get-second-of-all-suite)
 (run-tests convert-let-suite)
